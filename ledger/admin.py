95755e3fe6205faac7583f0123680d7b7551573f
Cobble-Gen <ianmercadopvt@gmail.com>
2025-02-27 21:54:34 +0800

    Implemented models for Recipes and RecipeList

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    inlines = [RecipeIngredientInline]
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
