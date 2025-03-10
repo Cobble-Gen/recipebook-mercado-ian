4047eda3c264f98a37467b5bc4591c62caeb9e9c
Cobble-Gen <ianmercadopvt@gmail.com>
2025-03-10 20:12:06 +0800

    Added profile admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Profile
 
 from .models import Profile
 
@@ -10,9 +11,15 @@ class ProfileInline(admin.StackedInline):
     can_delete = False
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+
 class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInline, ]
 
 
 admin.site.unregister(User)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(User, UserAdmin)
+
